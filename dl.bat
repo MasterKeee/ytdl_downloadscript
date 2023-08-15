77b5b703fa4ebc610ee86e3fc683b6c87425d700
柯大师 <63624079+MasterKeee@users.noreply.github.com>
2023-08-15 18:32:28 +0800

    Add files via upload

---
+++ dl.bat
@@ -0,0 +1,35 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM 提示用户输入下载路径
+set /p "output_dir=please enter your output directory: "
+
+REM 检查用户是否输入了下载路径
+if "%output_dir%"=="" (
+    echo Error: No output directory entered.
+    pause
+    exit /b
+)
+
+REM 创建下载路径
+mkdir "%output_dir%" >nul 2>nul
+
+REM 检查是否拖放了文件
+if "%~1"=="" (
+    echo Error: No file dropped onto the script.
+    echo Please drag and drop a text file containing video links onto the script.
+    pause
+    exit /b
+)
+
+REM 读取文本文件
+set "file=%~1"
+
+REM 下载视频
+for /f "usebackq delims=" %%A in ("%file%") do (
+    echo Downloading: %%A
+    youtube-dl --output "%output_dir%\%%(title)s.%%(ext)s" "%%A"
+)
+
+echo Download completed.
+pause
